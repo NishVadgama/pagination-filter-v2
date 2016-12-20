0462b9122f019c5ad02d5ffaeffd25db7f26acd8
Nish Vadgama <nishornash@gmail.com>
2016-12-20 13:20:13 -0500

    Changed for loop to While loop
    -While loop dynamcially adds pagination depending on number of students

--- pagination-filter-v2/app.js
+++ pagination-filter-v2/app.js
@@ -1,15 +1,14 @@
-//The page includes a list of 54 students.
-  //display 10 students per page,
-  // I think i have to use a jquery plug in here to add pagination
-  //and add buttons to jump between the lists of 10 students.
+//   Meets expectations
+//1. onLoad, show first 10 students only
+//2. Calculate number of pages needed
+//3. add page links/numbers dynamically
+//4. When user clicks on a page, students for that page should show
+//5. Program should work for any number of students
 
-//Your solution should work for any number of student listings,
-//whether there were 5, 25, 55 or 100 student listings.
-
-//<div class="page-header cf">  -- Insert Search Functionality
-
-//<ul class="student-list">  - parent of student listings
-
+// Exceeds expectations
+//1. include a searchable function
+//2. when search button is clicked, it will return search inputs text
+//3. if no matches found, return 'error message'
 
 const studentList = document.getElementsByClassName('student-list'); // The UL that holds on the student li's
 const studentItem = document.getElementsByClassName('student-item'); //This holds each student section block
@@ -20,35 +19,16 @@ const numberOfLinks = Math.floor(numberOfStudents/itemsPerPage);
 // The below code will only show the first 10 students on loading
 $(".student-item").slice(itemsPerPage).hide();
 
-//creating pagination to html  (This creates the div + UL)
+//creating tree/html to DOM  (This creates the div + UL)
 var $pagination = $("<div class='pagination'></div>");
 $($pagination).append("<ul class ='paginationUL'></ul>");
 $(".student-list").after($pagination);
 
 //creates a pagination depending on the number of students (increments of 10)
 var pageNumber = 0;
-for(var i = 0; i < numberOfLinks.length; i++){
-  $(".pagination ul").append("<li><a class='active' href='#'>" +pageNumber[i] + "</a></li>");
-  pageNumber += 1;
+while(numberOfLinks > pageNumber){
+pageNumber++;
+ $(".pagination ul").append("<li><a class='' href='#'>" +pageNumber + "</a></li>");
 }
 
-// //the li tag with the paginatino links will be added dynamically throgh a function
-//  $(".pagination ul").append("<li><a class='active' href='#'> 1</a></li>");
-// if(numberOfStudents > 10) {
-//  $(".pagination ul").append("<li><a class='active' href='#'> 2</a></li>");
-// }
-// if(numberOfStudents > 20) {
-//   $(".pagination ul").append("<li><a class='active' href='#'> 3</a></li>");
-// }
-// if(numberOfStudents > 30) {
-// $(".pagination ul").append("<li><a class='active' href='#'> 4</a></li>");
-// }
-// if(numberOfStudents > 40) {
-// $(".pagination ul").append("<li><a class='active' href='#'> 5</a></li>");
-// }
-// if(numberOfStudents > 50) {
-// $(".pagination ul").append("<li><a class='active' href='#'> 6</a></li>");
-// }
-// if(numberOfStudents > 60) {
-// $(".pagination ul").append("<li><a class='active' href='#'> 7</a></li>");
-// }
+const getPageNumber = document.getElementsByClassName('paginationUL');
